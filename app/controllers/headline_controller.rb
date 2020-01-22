599863d48bc8513ad2507f6359a85a12057fbe87
Heli Sivunen <heli@mooncat.tv>
2020-01-22 08:04:20 +0000

    Modify array search to satisfy Heroku.

--- app/controllers/headline_controller.rb
+++ app/controllers/headline_controller.rb
@@ -12,13 +12,11 @@ class HeadlineController < ApplicationController
       return render action: :index
     end
 
-  p  @headlines = headlines.to_a[1][1].pluck("results")[0].pluck("title", "location")
+  p  @headlines = headlines.to_a[1][1].pluck("results").first.pluck("title", "location")
     # wrong: headlines.to_h.map {|x| x.values_at("title")}
     # wrong: .fetch_values("title", "location")
     # wrong:  .to_a[0].to_h.pluck[("title") ("location")]
     # works: headlines.to_a[1][1].pluck("results")[0].pluck("title", "location").to_h
-
-
   end
 
   private
