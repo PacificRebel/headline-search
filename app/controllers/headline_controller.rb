9c8a2d87eb59e2cb6b9ca2a47aba2121f0d12aa2
Heli Sivunen <heli@mooncat.tv>
2020-09-15 17:38:11 +0100

    Upgrade Yarn

--- app/controllers/headline_controller.rb
+++ app/controllers/headline_controller.rb
@@ -14,6 +14,7 @@ class HeadlineController < ApplicationController
 
       if @headlines_abridged.present?
          @plucked_headlines = @headlines_abridged.first["results"].pluck("title", "location").to_h
+         #is this follwing line correct? Why is Heroku rendering headline.index.html.erb when there's a result?
          return render action: :search
       else
          # flash[:alert] = 'No headlines coming up under that search word.'
